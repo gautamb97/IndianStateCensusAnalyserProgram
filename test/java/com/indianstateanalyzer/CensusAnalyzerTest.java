59be51d54cd34a9747b3ebee8e1a810828d3a999
gautamb97 <www.gautam971997@gmail.com>
2021-03-10 16:54:24 +0530

    Added:Tested for given file has wrong file type

--- test/java/com/indianstateanalyzer/CensusAnalyzerTest.java
+++ test/java/com/indianstateanalyzer/CensusAnalyzerTest.java
@@ -9,7 +9,7 @@ public class CensusAnalyzerTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
-
+    private static final String WRONG_CSV_FILE_TYPE ="./IndiaStateCensusData.pdf";
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
@@ -30,6 +30,18 @@ public class CensusAnalyzerTest {
             censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFileType_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_TYPE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals("Wrong File Type",CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
     }
 
